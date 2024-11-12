b1e6f1b9f7b13117d78dc5d3df32d96d9f20163c
cploujoux <ch.ploujoux@gmail.com>
2024-11-12 10:19:10 -0500

    Use client credentials instead of credentials

--- agents/langchain-chat-completions/bl_auth.py
+++ agents/langchain-chat-completions/bl_auth.py
@@ -8,8 +8,8 @@ from .bl_config import BL_CONFIG
 async def retrieve_jwt():
     global BL_CONFIG
 
-    credentials = BL_CONFIG.get('credentials')
-    headers = { "Authorization": f"Basic {credentials}" }
+    client_credentials = BL_CONFIG.get('client_credentials')
+    headers = { "Authorization": f"Basic {client_credentials}" }
     body = { "grant_type": "client_credentials" }
     response = requests.post(f"{BL_CONFIG['base_url']}/oauth/token", headers=headers, json=body)
     if response.status_code == 200:
@@ -23,13 +23,13 @@ async def auth():
     # If jwt or api_key is set, we don't need to retrieve jwt dynamically
     if BL_CONFIG.get('jwt') or BL_CONFIG.get('api_key'):
         return
-    if BL_CONFIG.get('credentials'):
+    if BL_CONFIG.get('client_credentials'):
         await retrieve_jwt()
         return
-    raise Exception("No beamlit credentials found, need JWT or API Key or credentials")
+    raise Exception("No beamlit client_credentials found, need JWT or API Key or client_credentials")
 
 async def auth_loop():
-    if BL_CONFIG.get('credentials'):
+    if BL_CONFIG.get('client_credentials'):
         while True:
             await asyncio.sleep(BL_CONFIG['jwt_expires_in'] - 10)
             await auth()
