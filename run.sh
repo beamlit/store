7417895f387395c73e5e45e1f889cf05645eda85
cploujoux <ch.ploujoux@gmail.com>
2025-01-30 17:11:06 -0800

    Add uv lock

---
+++ run.sh
@@ -0,0 +1,10 @@
+# BL_FUNCTIONS="dall-e-2" \
+BL_CLIENT_CREDENTIALS="bWFpbjo1R0pUR0FGUTdPTTRKNE9BWlBTVFIyMFZNVDIxQUM4SA==" \
+BL_AUTHENTICATION_CLIENT_CREDENTIALS="bWFpbjo1R0pUR0FGUTdPTTRKNE9BWlBTVFIyMFZNVDIxQUM4SA==" \
+BL_WORKSPACE="main" \
+BL_BASE_URL="https://api.beamlit.dev/v0" \
+BL_RUN_URL="https://run.beamlit.dev" \
+BL_TYPE="agent" \
+BL_NAME="agent-gpt-4o-mini" \
+BL_ENVIRONMENT="production" \
+bl serve --hotreload --module src.agents.beamlit-agent.main.main
