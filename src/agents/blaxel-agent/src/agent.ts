fe701d149b8dc35be7f22f643680c21349df3196
drappier-charles <cdrappier@beamlit.com>
2025-04-02 18:16:50 +0200

    Switch the store agent to new sdk

--- src/agents/blaxel-agent/src/agent.ts
+++ src/agents/blaxel-agent/src/agent.ts
@@ -1,103 +1,58 @@
-import {
-  getAgent,
-  getChatModel,
-  getDefaultThread,
-  getSettings,
-  logger,
-  newClient,
-  OpenAIVoiceReactAgent,
-  wrapAgent,
-} from "@blaxel/sdk";
-import { HumanMessage } from "@langchain/core/messages";
-import { FastifyRequest } from "fastify";
-import { v4 as uuidv4 } from "uuid";
+import { Agent, blModel, blTools, env, getAgent, logger } from '@blaxel/sdk';
+import { streamText } from 'ai';
+interface Stream {
+  write: (data: string) => void;
+  end: () => void;
+}
 
-type InputType = {
-  inputs: string | null;
-  input: string | null;
-};
-
-const websocketHandler = async (
-  ws: WebSocket,
-  request: FastifyRequest,
-  args: any
-) => {
-  const { agent, functions } = args;
-  logger.info("Websocket connected, request: ", request);
-
-  agent.bindTools(functions);
-  await agent.connect(ws, ws.send.bind(ws));
-  ws.onclose = () => {
-    logger.info("Websocket closed");
-  };
-};
-
-const requestHandler = async (request: FastifyRequest, args: any) => {
-  const { agent } = args;
-  const body = (await request.body) as InputType;
-  const thread_id = getDefaultThread(request) || uuidv4();
-  const input = body.inputs || body.input || "";
-  const responses: any[] = [];
-
-  const stream = await agent.stream(
-    { messages: [new HumanMessage(input)] },
-    { configurable: { thread_id } }
-  );
-
-  for await (const chunk of stream) {
-    responses.push(chunk);
-  }
-  const content = responses[responses.length - 1];
-  return content.agent.messages[content.agent.messages.length - 1].content;
-};
-
-async function runAgent(retry: number = 0) {
+async function getTools(agent: Agent) {
   try {
-    const settings = await getSettings();
-    const client = await newClient();
-    const { data: agent } = await getAgent({
-      client,
-      path: { agentName: settings.name },
-    });
-    if (!agent) {
-      throw new Error("Agent not found");
-    }
-    const chat = await getChatModel(agent?.spec?.model || "");
-
-    let prompt = agent?.spec?.prompt;
-    if (typeof process.env.BL_PROMPT === "string") {
-      prompt = process.env.BL_PROMPT;
-    }
-
-    const config = {
-      agent: {
-        metadata: {
-          name: agent?.metadata?.name,
-        },
-        spec: {
-          description: agent?.spec?.description,
-          prompt,
-          model: agent?.spec?.model,
-          agentChain: agent?.spec?.agentChain,
-        },
-      },
-      remoteFunctions: agent?.spec?.functions,
-    };
-    if (chat instanceof OpenAIVoiceReactAgent) {
-      return wrapAgent(websocketHandler, config);
-    }
-    return wrapAgent(requestHandler, config);
+    return await blTools(agent.spec?.functions || []).ToVercelAI()
   } catch (error) {
-    if (retry > 10) {
-      throw error;
-    }
-    const stackTrace = error instanceof Error ? error.stack : String(error);
-    logger.error(`Error running agent: ${stackTrace}`);
-    logger.info(`Retrying agent... Retry number:${retry}`);
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    return runAgent(retry + 1);
+    logger.error(error)
+    throw new Error(`Error connecting to mcp servers, if the agent was just created, it may need an attached mcp server to be deployed.`)
   }
 }
-export const agent = async () => {
-  return runAgent();
-};
+
+async function getModel(agent: Agent) {
+  try {
+    return await blModel(agent.spec?.model || "").ToVercelAI()
+  } catch (error) {
+    logger.error(error)
+    throw new Error(`Error connecting to model.`)
+  }
+}
+
+export default async function agent(input: string, stream: Stream) {
+  // @ts-ignore
+  const agentName = env.BL_NAME || "";
+  let {data:agent} = await getAgent({path:{
+    agentName
+  }})
+  if(!agent) {
+    throw new Error(`Agent not found : ${agentName}`)
+  }
+  let [tools,model] = await Promise.all([
+    getTools(agent),
+    getModel(agent)
+  ])
+  let system = agent.spec?.prompt || ""
+  
+  const response = streamText({
+    experimental_telemetry: { isEnabled: true },
+    model,
+    // @ts-ignore
+    tools,
+    system,
+    messages: [
+      { role: 'user', content: input }
+    ],
+    maxSteps: 5,
+  });
+
+  for await (const delta of response.textStream) {
+    stream.write(delta);
+  }
+
+  stream.end();
+}
